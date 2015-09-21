773e99998806fd3a7079d4cb9dba122f2e3521fa
Zhike Chan <zk.chan007@gmail.com>
2015-09-21 22:07:46 +0800

    modify to get img from cn.bing.com

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,12 +4,13 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-urls=( $(curl -s http://www.bing.com | \
-    grep -Eo "url:'.*?'" | \
-    sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
+urls=( $(curl -s http://cn.bing.com | \
+    grep -Eo "g_img={url:'.*?'" | \
+    sed -e "s/g_img={url:'\([^']*\)'.*/\1/" | \
     sed -e "s/\\\//g") )
 
 for p in ${urls[@]}; do
+    echo $p
     filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
     if [ ! -f $PICTURE_DIR/$filename ]; then
         echo "Downloading: $filename ..."
