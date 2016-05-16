0972864a559f4a279e2866bb0f009a1eb9171486
Zhike Chan <zk.chan007@gmail.com>
2016-05-16 17:31:44 +0800

    fix: get image link

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+# set -x
 
 PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
+# g_img={url: "http:\/\/s.cn.bing.net\/az\/hprichbg\/rb\/PuttyBeach_ZH-CN8972640560_1920x1080.jpg",
+# urls=( $(curl -s http://cn.bing.com | \
+#     grep -Eo "g_img={url: \".*?\"" | \
+#     sed -e "s/g_img={url: \"\([^']*\)\".*/\1/" | \
+#     sed -e "s/\\\//g") )
 urls=( $(curl -s http://cn.bing.com | \
-    grep -Eo "g_img={url:'.*?'" | \
-    sed -e "s/g_img={url:'\([^']*\)'.*/\1/" | \
+    sed -En "s/.*g_img={url: *[\"'](http.*jpg)[\"'],id.*/\1/p" | \
     sed -e "s/\\\//g") )
 
 for p in ${urls[@]}; do
@@ -19,3 +24,7 @@ for p in ${urls[@]}; do
         echo "Skipping: $filename ..."
     fi
 done
+
+# curl -s http://cn.bing.com | grep -Eo "g_img={url:'.*?'" | sed -e "s/g_img={url:'\([^']*\)'.*/\1/" | sed -e "s/\\\//g"
+# curl -s http://cn.bing.com | grep -Eo "g_img={url: \".*?\"" | sed -e "s/g_img={url:'\([^']*\)'.*/\1/" | sed -e "s/\\\//g"
+# curl -s http://cn.bing.com | sed -En "s/.*g_img={url: *[\"'](http.*jpg)[\"'],id.*/\1/p"
