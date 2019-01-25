e10338197111252f9bbf6af55afc75f60f9d04d0
Zhike Chen <zk.chan007@gmail.com>
2019-01-25 11:08:41 +0800

    update download wallpaper script

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -5,7 +5,7 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-urls=( $(curl -s http://www.bing.com | sed -En 's/.*bgImg" src="(.*)/\1/p' | cut -d\" -f1) )
+urls=( $(curl -s http://www.bing.com | sed -En 's/.*bgLink" rel="preload" href="(.*)/\1/p' | cut -d\" -f1) )
 
 for p in ${urls[@]}; do
     echo $p
